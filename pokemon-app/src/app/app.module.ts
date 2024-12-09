8143fb3cfcc22b04b56a9e1c22f67b9a40f8890a
T0mm4s0Bry brignoli <brignoli.tommaso@iisgalvanimi.edu.it>
2024-12-09 12:44:47 +0000

    ho aggiunto httpclientmodule

---
+++ pokemon-app/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
