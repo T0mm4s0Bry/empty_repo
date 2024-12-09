719112451d8dc31f27e40a36601dfa26b133c45c
T0mm4s0Bry brignoli <brignoli.tommaso@iisgalvanimi.edu.it>
2024-12-09 13:08:42 +0000

    adesso ho creato un terzo componente details

--- pokemon-app/src/app/home/home.component.ts
+++ pokemon-app/src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  types: any[] = [];
 
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnInit(): void {
+    this.pokemonService.getTypes().subscribe(data => {
+      this.types = data.results;
+    });
+  }
 }
--- pokemon-app/src/app/type-details/type-details.component.ts
+++ pokemon-app/src/app/type-details/type-details.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-type-details',
   templateUrl: './type-details.component.html',
   styleUrls: ['./type-details.component.css']
 })
-export class TypeDetailsComponent {
+export class TypeDetailsComponent implements OnInit {
+  typeDetails: any;
+  typeName: string = '';
 
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.typeName = params.get('type') || '';
+      if (this.typeName) {
+        this.pokemonService.getTypeDetails(this.typeName).subscribe(data => {
+          this.typeDetails = data;
+        });
+      }
+    });
+  }
 }
