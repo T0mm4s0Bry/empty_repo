89f63b5f87409bc42ef60278aaf27b43c6622f14
T0mm4s0Bry brignoli <brignoli.tommaso@iisgalvanimi.edu.it>
2024-12-09 12:46:54 +0000

    aggiunti gli import

--- pokemon-app/src/app/app.module.ts
+++ pokemon-app/src/app/app.module.ts
@@ -1,4 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
